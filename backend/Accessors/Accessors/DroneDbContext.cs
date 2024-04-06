8a9722a638dc51270bfe3150e9d73c2285135958
ecthelionvi <rsears2@huskers.unl.edu>
2024-04-06 18:42:49 -0500

    Removed Folders

--- backend/Accessors/Accessors/DroneDbContext.cs
+++
@@ -1,9 +0,0 @@
-namespace Accessors;
-
-public class DroneDbContext
-{
-    public static void Main(string[] args)
-    {
-        
-    }
-}
--- backend/Service/Models/DroneModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Service.Models;
-
-public class DroneModel
-{
-     public string Id { get; set; }
-     public string Status { get; set; }
-     public string Destination { get; set; }
-}
