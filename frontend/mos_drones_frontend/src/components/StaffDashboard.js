1af2ea62ac55c8735328ec6e220f1df84bc51b30
ecthelionvi <rsears2@huskers.unl.edu>
2024-04-06 21:58:15 -0500

    Move frontend changes into frontend folder

---
+++ frontend/mos_drones_frontend/src/components/StaffDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StaffDashboard = () => {
+  return (
+    <div>
+      <h2>Welcome, Staff!</h2>
+      <p>This is the staff dashboard.</p>
+    </div>
+  );
+};
+
+export default StaffDashboard;
