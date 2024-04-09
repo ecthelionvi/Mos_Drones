072f077bf9ed19d355382fb5eda21e0a82a63c2d
Angie Zheng <111665983+angzheng22@users.noreply.github.com>
2024-04-09 13:24:23 -0500

    DB connection

---
+++ backend/Accessors/DatabaseConnection/DbConnection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SqlConn
+{
+    class DatabaseConnection
+    {
+        public static void TestDatabaseConnection()
+        {
+            string connection = "Server=localhost,1433;Database=Mos_Drones;User Id=sa;Password=password123;";
+
+            SqlConnection conn = new SqlConnection(connection);
+            Console.WriteLine("Getting Connection ...");
+            try
+            {
+                Console.WriteLine("Opening Connection ...");
+                conn.Open();
+                Console.WriteLine("Connection successful!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            Console.Read();
+
+        }
+        static void Main(string[] args)
+        {
+            TestDatabaseConnection();
+        }
+    }
+}
