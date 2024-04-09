7a4529d8b9e7972cbfbf8fc625044b48c8e0e6ed
Angie Zheng <111665983+angzheng22@users.noreply.github.com>
2024-04-09 13:26:17 -0500

    Models

---
+++ backend/Managers/Models/DepotModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Depot
+{
+    private int depotId { get; set; }
+    private Address depotAddress { get; set; }
+
+    public Depot(int depotId, Address depotAddress)
+    {
+        this.depotId = depotId;
+        this.depotAddress = depotAddress;
+    }
+
+}
---
+++ backend/Managers/Models/OrderModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Order
+{
+    private int orderId { get; set; }
+    private string packageId { get; set; }
+    private DateTime shipDate { get; set; }
+    private User user { get; set; }
+    private Address shippedFrom { get; set; }
+    private Address shippedTo { get; set; }
+
+    public Order(int orderId, string packageId, DateTime shipDate, User user, Address shippedFrom, Address shippedTo)
+    {
+        this.orderId = orderId;
+        this.packageId = packageId;
+        this.shipDate = shipDate;
+        this.user = user;
+        this.shippedFrom = shippedFrom;
+        this.shippedTo = shippedTo;
+    }
+}
