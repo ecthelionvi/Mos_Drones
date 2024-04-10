cf02c3ad28e79daf69a10c2132ba20c0f5b7b701
ecthelionvi <rsears2@huskers.unl.edu>
2024-04-09 22:09:25 -0500

    Update HomePage

--- frontend/mos_drones_frontend/src/components/HomePage.js
+++ frontend/mos_drones_frontend/src/components/HomePage.js
@@ -1,28 +1,28 @@
-import React, { useState, useRef, useEffect } from 'react';
-import drone from '../images/drone.png';
-import logo from '../images/logo.png';
-import mo from '../images/mo.png';
-import pkg from '../images/package.png'
-import drone_pkg from '../images/drone-pkg.png'
-import '../styles/HomePage.css';
-import PackageGrid from './PackageGrid'
-import ServiceArea from '../components/SerivceArea';
-import DroneGrid from '../components/DroneGrid';
-import { useJsApiLoader, Autocomplete } from '@react-google-maps/api';
-import { NavLink, useNavigate } from 'react-router-dom';
+import React, { useState, useRef, useEffect } from "react";
+import drone from "../images/drone.png";
+import logo from "../images/logo.png";
+import mo from "../images/mo.png";
+import pkg from "../images/package.png";
+import drone_pkg from "../images/drone-pkg.png";
+import "../styles/HomePage.css";
+import PackageGrid from "./PackageGrid";
+import ServiceArea from "../components/SerivceArea";
+import DroneGrid from "../components/DroneGrid";
+import { useJsApiLoader, Autocomplete } from "@react-google-maps/api";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const HomePage = ({ loggedIn, onLogout, setLoggedIn, role }) => {
-  const [activeTrackingTab, setActiveTrackingTab] = useState('tracking');
-  const [activeHeaderTab, setActiveHeaderTab] = useState(loggedIn ? 'dashboard' : 'home');
-  const [trackingNumber, setTrackingNumber] = useState('');
+  const [activeTrackingTab, setActiveTrackingTab] = useState("tracking");
+  const [activeHeaderTab, setActiveHeaderTab] = useState(loggedIn ? "dashboard" : "home");
+  const [trackingNumber, setTrackingNumber] = useState("");
   const fromAutocompleteRef = useRef(null);
   const toAutocompleteRef = useRef(null);
   const navigate = useNavigate();
 
   const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: 'AIzaSyCfTn6UID_1mfAbHLjaFNsgAww13JewQzE',
-    libraries: ['places'],
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyCfTn6UID_1mfAbHLjaFNsgAww13JewQzE",
+    libraries: ["places"],
   });
 
   const handleTrackButtonClick = () => {
@@ -32,7 +32,7 @@ const HomePage = ({ loggedIn, onLogout, setLoggedIn, role }) => {
   };
 
   const handleEnterKey = (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       handleTrackButtonClick();
     }
   };
@@ -53,9 +53,8 @@ const HomePage = ({ loggedIn, onLogout, setLoggedIn, role }) => {
   };
 
   useEffect(() => {
-    setActiveHeaderTab(loggedIn ? 'dashboard' : 'home');
+    setActiveHeaderTab(loggedIn ? "dashboard" : "home");
   }, [loggedIn]);
-
 
   return (
     <div className="homepage-body">
@@ -65,31 +64,31 @@ const HomePage = ({ loggedIn, onLogout, setLoggedIn, role }) => {
             <img className="header__logo" src={logo} alt="Mo's Drones Logo" />
             <div className="header__tabs">
               <div
-                className={`header__tab ${activeHeaderTab === 'home' ? 'header__tab--active' : ''}`}
-                onClick={() => handleHeaderTabClick('home')}
+                className={`header__tab ${activeHeaderTab === "home" ? "header__tab--active" : ""}`}
+                onClick={() => handleHeaderTabClick("home")}
               >
                 <span className="header__tab__span">Home</span>
                 <div className="header__tab-underline"></div>
               </div>
               <div
-                className={`header__tab ${activeHeaderTab === 'map' ? 'header__tab--active' : ''}`}
-                onClick={() => handleHeaderTabClick('map')}
+                className={`header__tab ${activeHeaderTab === "map" ? "header__tab--active" : ""}`}
+                onClick={() => handleHeaderTabClick("map")}
               >
                 <span className="header__tab__span">Service Area</span>
                 <div className="header__tab-underline"></div>
               </div>
               {loggedIn && (
                 <div
-                  className={`header__tab ${activeHeaderTab === 'dashboard' ? 'header__tab--active' : ''}`}
-                  onClick={() => handleHeaderTabClick('dashboard')}
+                  className={`header__tab ${activeHeaderTab === "dashboard" ? "header__tab--active" : ""}`}
+                  onClick={() => handleHeaderTabClick("dashboard")}
                 >
                   <span className="header__tab__span">Dashboard</span>
                   <div className="header__tab-underline"></div>
                 </div>
               )}
               <div
-                className={`header__tab ${activeHeaderTab === 'about' ? 'header__tab--active' : ''}`}
-                onClick={() => handleHeaderTabClick('about')}
+                className={`header__tab ${activeHeaderTab === "about" ? "header__tab--active" : ""}`}
+                onClick={() => handleHeaderTabClick("about")}
               >
                 <span className="header__tab__span">About</span>
                 <div className="header__tab-underline"></div>
@@ -106,26 +105,28 @@ const HomePage = ({ loggedIn, onLogout, setLoggedIn, role }) => {
             )}
           </header>
           <main className="main-content">
-            <section className={`home-section ${activeHeaderTab === 'home' ? '' : 'hidden'}`}>
+            <section className={`home-section ${activeHeaderTab === "home" ? "" : "hidden"}`}>
               <section className="tracking-section">
                 <div className="tracking-section__tabs">
                   <div
-                    className={`tracking-section__tab ${activeTrackingTab === 'tracking' ? 'tracking-section__tab--active' : ''}`}
-                    onClick={() => handleTrackingTabClick('tracking')}
+                    className={`tracking-section__tab ${activeTrackingTab === "tracking" ? "tracking-section__tab--active" : ""}`}
+                    onClick={() => handleTrackingTabClick("tracking")}
                   >
                     <span className="tracking-section__tab__span">Tracking</span>
                     <div className="tracking-section__tab-underline"></div>
                   </div>
                   <div
-                    className={`tracking-section__tab ${activeTrackingTab === 'request-delivery' ? 'tracking-section__tab--active' : ''}`}
-                    onClick={() => handleTrackingTabClick('request-delivery')}
+                    className={`tracking-section__tab ${activeTrackingTab === "request-delivery" ? "tracking-section__tab--active" : ""}`}
+                    onClick={() => handleTrackingTabClick("request-delivery")}
                   >
                     <span className="tracking-section__tab__span">Request Delivery</span>
                     <div className="tracking-section__tab-underline"></div>
                   </div>
                 </div>
                 <div className="tracking-section__content">
-                  <div className={`tracking-section__tracking ${activeTrackingTab === 'tracking' ? '' : 'hidden'}`}>
+                  <div
+                    className={`tracking-section__tracking ${activeTrackingTab === "tracking" ? "" : "hidden"}`}
+                  >
                     <input
                       type="text"
                       className="tracking-section__tracking-input"
@@ -134,43 +135,69 @@ const HomePage = ({ loggedIn, onLogout, setLoggedIn, role }) => {
                       onChange={(e) => setTrackingNumber(e.target.value)}
                       onKeyPress={handleEnterKey}
                     />
-                    <button className="tracking-section__tracking-btn" onClick={handleTrackButtonClick}>
+                    <button
+                      className="tracking-section__tracking-btn"
+                      onClick={handleTrackButtonClick}
+                    >
                       Track &raquo;
                     </button>
                   </div>
-                  <div className={`tracking-section__delivery ${activeTrackingTab === 'request-delivery' ? '' : 'hidden'}`}>
+                  <div
+                    className={`tracking-section__delivery ${activeTrackingTab === "request-delivery" ? "" : "hidden"}`}
+                  >
                     {isLoaded && (
                       <>
                         <Autocomplete
-                          onLoad={(autocomplete) => { fromAutocompleteRef.current = autocomplete; }}
+                          onLoad={(autocomplete) => {
+                            fromAutocompleteRef.current = autocomplete;
+                          }}
                           onPlaceChanged={() => onPlaceChanged(fromAutocompleteRef)}
                         >
-                          <input type="text" className="tracking-section__delivery-from" placeholder="From" />
+                          <input
+                            type="text"
+                            className="tracking-section__delivery-from"
+                            placeholder="From"
+                          />
                         </Autocomplete>
                         <Autocomplete
-                          onLoad={(autocomplete) => { toAutocompleteRef.current = autocomplete; }}
+                          onLoad={(autocomplete) => {
+                            toAutocompleteRef.current = autocomplete;
+                          }}
                           onPlaceChanged={() => onPlaceChanged(toAutocompleteRef)}
                         >
-                          <input type="text" className="tracking-section__delivery-to" placeholder="To" />
+                          <input
+                            type="text"
+                            className="tracking-section__delivery-to"
+                            placeholder="To"
+                          />
                         </Autocomplete>
                       </>
                     )}
-                    <button className="tracking-section__delivery-btn">Request Delivery &raquo;</button>
+                    <button className="tracking-section__delivery-btn">
+                      Request Delivery &raquo;
+                    </button>
                   </div>
                 </div>
               </section>
               <div className="content-wrapper">
                 <img className="main-image-top" src={drone} alt="Drone with Package" />
-                <h1 className="main-title">World-Class Services<br />You Can Count On</h1>
+                <h1 className="main-title">
+                  World-Class Services
+                  <br />
+                  You Can Count On
+                </h1>
                 <div className="title-underline"></div>
                 <p className="main-slogan">Nebraska Skies, Swift Supplies</p>
               </div>
             </section>
-            <section className={`about-section ${activeHeaderTab === 'about' ? '' : 'hidden'}`}>
+            <section className={`about-section ${activeHeaderTab === "about" ? "" : "hidden"}`}>
               <h2>About Us</h2>
               <div className="about-section__underline"></div>
               <p className="about-section-paragraph">
-                Welcome to Mo's Drones, where innovation meets convenience in the world of delivery services. At Mo's Drones, we're passionate about revolutionizing the way goods are transported, bringing unparalleled speed, efficiency, and reliability to your doorstep.
+                Welcome to Mo's Drones, where innovation meets convenience in the world of delivery
+                services. At Mo's Drones, we're passionate about revolutionizing the way goods are
+                transported, bringing unparalleled speed, efficiency, and reliability to your
+                doorstep.
               </p>
               <div className="image-container">
                 <img className="about-image-top" src={mo} alt="Mr. Mo" />
@@ -179,8 +206,10 @@ const HomePage = ({ loggedIn, onLogout, setLoggedIn, role }) => {
                 </div>
               </div>
             </section>
-            <section className={`dashboard-section ${activeHeaderTab === 'dashboard' ? '' : 'hidden'}`}>
-              {role === 'staff' ? (
+            <section
+              className={`dashboard-section ${activeHeaderTab === "dashboard" ? "" : "hidden"}`}
+            >
+              {role === "staff" ? (
                 <>
                   <h2>Drones</h2>
                   <div className="dashboard-section__underline-drone"></div>
@@ -196,7 +225,10 @@ const HomePage = ({ loggedIn, onLogout, setLoggedIn, role }) => {
                 </>
               )}
             </section>
-            <section className={`map-section ${activeHeaderTab === 'map' ? '' : 'hidden'}`} key={activeHeaderTab}>
+            <section
+              className={`map-section ${activeHeaderTab === "map" ? "" : "hidden"}`}
+              key={activeHeaderTab}
+            >
               <h2>Service Area</h2>
               <div className="map-section__underline"></div>
               <ServiceArea />
