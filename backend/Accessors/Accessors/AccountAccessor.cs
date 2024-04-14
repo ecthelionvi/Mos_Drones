55c36ca5f49275336ddb34c080340510a76a7d61
Angie Zheng <111665983+angzheng22@users.noreply.github.com>
2024-04-14 18:14:38 -0500

    Added AccountAccessor.cs

---
+++ backend/Accessors/Accessors/AccountAccessor.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.SqlClient;
+using System.Net;
+using Managers.Models;
+
+namespace Accessors.Accessors
+{
+    public class AccountAccessor
+    {
+        public static Account GetAccountWithEmail(string email)
+        {
+            Account account = null;
+            Address accountAddress = null;
+
+            string query = "SELECT * FROM Account WHERE email = @Email";
+
+            SqlConnection connection = ConnectionAccessor.ConnectionAccessor.GetConnection();
+
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Email", email);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows && reader.Read())
+                {
+                    int accountId = reader.GetInt32(reader.GetOrdinal("accountId"));
+                    string firstName = reader.GetString(reader.GetOrdinal("first_name"));
+                    string lastName = reader.GetString(reader.GetOrdinal("last_name"));
+                    string password = reader.GetString(reader.GetOrdinal("password"));
+                    int addressId = reader.GetInt32(reader.GetOrdinal("addressId"));
+
+                    accountAddress = AddressAccessor.GetAddress(addressId);
+
+                    account = new Account(accountId, firstName, lastName, email, password, accountAddress);
+                }
+
+                reader.Close();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"SQL Exception: {ex.Message}");
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return account;
+        }
+    }
+}
