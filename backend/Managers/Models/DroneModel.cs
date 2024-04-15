3a5e5be224fbc2bc34dd4bb566de0dc06fe53908
Angie Zheng <111665983+angzheng22@users.noreply.github.com>
2024-04-15 18:35:34 -0500

    Updated DroneModel.cs
    ToString() and made currentDepot nullable

--- backend/Managers/Models/DroneModel.cs
+++ backend/Managers/Models/DroneModel.cs
@@ -7,14 +7,19 @@ namespace Managers.Models
         public int droneId { get; set; }
         public string transitStatus { get; set; }
         public Order order { get; set; }
-        public Depot currentDepot { get; set; }
+        public Depot? currentDepot { get; set; }
 
-        public Drone(int droneId, string transitStatus, Order order, Depot currentDepot)
+        public Drone(int droneId, string transitStatus, Order order, Depot? currentDepot)
         {
             this.droneId = droneId;
             this.transitStatus = transitStatus;
             this.order = order;
             this.currentDepot = currentDepot;
+        }
+
+        public override string ToString()
+        {
+            return $"droneId: {droneId}\nTransit Status: {transitStatus}\n{order}{currentDepot}";
         }
     }
 }
