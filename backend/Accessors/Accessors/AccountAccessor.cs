fd5c9224dd5a5399642922a4142ac8cdf49f5c5b
Angie Zheng <111665983+angzheng22@users.noreply.github.com>
2024-04-15 12:54:54 -0500

    Updated AccountAccessor.cs

--- backend/Accessors/Accessors/AccountAccessor.cs
+++ backend/Accessors/Accessors/AccountAccessor.cs
@@ -1,4 +1,4 @@
-﻿using System.Data.SqlClient;
+using System.Data.SqlClient;
 using System.Net;
 using Managers.Models;
 
@@ -6,6 +6,48 @@ namespace Accessors.Accessors
 {
     public class AccountAccessor
     {
+        public static Account GetAccountWithAccountId(int accountId)
+        {
+            Account account = null;
+            Address accountAddress = null;
+
+            string query = "SELECT * FROM Account WHERE accountId = @AccountId";
+
+            SqlConnection connection = ConnectionAccessor.ConnectionAccessor.GetConnection();
+
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@AccountId", accountId);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows && reader.Read())
+                {
+                    string firstName = reader.GetString(reader.GetOrdinal("first_name"));
+                    string lastName = reader.GetString(reader.GetOrdinal("last_name"));
+                    string email = reader.GetString(reader.GetOrdinal("email"));
+                    string password = reader.GetString(reader.GetOrdinal("password"));
+                    int addressId = reader.GetInt32(reader.GetOrdinal("addressId"));
+                    bool isAdmin = reader.GetBoolean(reader.GetOrdinal("isAdmin"));
+
+                    accountAddress = AddressAccessor.GetAddress(addressId);
+
+                    account = new Account(accountId, firstName, lastName, email, password, accountAddress,isAdmin);
+                }
+
+                reader.Close();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"SQL Exception: {ex.Message}");
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return account;
+        }
         public static Account GetAccountWithEmail(string email)
         {
             Account account = null;
@@ -28,10 +70,11 @@ namespace Accessors.Accessors
                     string lastName = reader.GetString(reader.GetOrdinal("last_name"));
                     string password = reader.GetString(reader.GetOrdinal("password"));
                     int addressId = reader.GetInt32(reader.GetOrdinal("addressId"));
+                    bool isAdmin = reader.GetBoolean(reader.GetOrdinal("isAdmin"));
 
                     accountAddress = AddressAccessor.GetAddress(addressId);
 
-                    account = new Account(accountId, firstName, lastName, email, password, accountAddress);
+                    account = new Account(accountId, firstName, lastName, email, password, accountAddress, isAdmin);
                 }
 
                 reader.Close();
