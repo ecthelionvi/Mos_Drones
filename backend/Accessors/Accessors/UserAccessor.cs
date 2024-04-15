b875ce958988dfd11e515be75a5f5f23feafcd99
Angie Zheng <111665983+angzheng22@users.noreply.github.com>
2024-04-15 00:03:48 -0500

    Added UserAccessor

---
+++ backend/Accessors/Accessors/UserAccessor.cs
@@ -0,0 +1,47 @@
+﻿using System.Data.SqlClient;
+using Managers.Models;
+
+namespace Accessors.Accessors
+{
+    public class UserAccessor
+    {
+        public static User GetUser(int userId)
+        {
+            User user = null;
+            Account userAccount = null;
+
+            string query = "SELECT * FROM [User] WHERE userId = @UserId";
+
+            SqlConnection connection = ConnectionAccessor.ConnectionAccessor.GetConnection();
+
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@UserId", userId);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows && reader.Read())
+                {
+                    bool isAdmin = reader.GetBoolean(reader.GetOrdinal("isAdmin"));
+                    int accountId = reader.GetInt32(reader.GetOrdinal("accountId"));
+
+                    userAccount = AccountAccessor.GetAccountWithAccountId(accountId);
+
+                    user = new User(userId, isAdmin, userAccount);
+                }
+
+                reader.Close();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"SQL Exception: {ex.Message}");
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return user;
+        }
+    }
+}
