0c23663a800c2e7786e24ecb3d9e55e9726d0e34
Angie Zheng <111665983+angzheng22@users.noreply.github.com>
2024-04-16 13:32:48 -0500

    Added ToString() to OrderDataModel.cs

--- backend/Accessors/DBModels/OrderDataModel.cs
+++ backend/Accessors/DBModels/OrderDataModel.cs
@@ -2,21 +2,25 @@ namespace Accessors.DBModels;
 
 public class OrderDataModel
 {
-        public int OrderId { get; set; }
-        public string PackageId { get; set; }
-        public DateTime ShipDate { get; set; }
-        public AccountDataModel Account { get; set; }
-        public AddressDataModel ShippedFrom { get; set; }
-        public AddressDataModel ShippedTo { get; set; }
+    public int OrderId { get; set; }
+    public string PackageId { get; set; }
+    public DateTime ShipDate { get; set; }
+    public AccountDataModel Account { get; set; }
+    public AddressDataModel ShippedFrom { get; set; }
+    public AddressDataModel ShippedTo { get; set; }
 
-        public OrderDataModel(int orderId, string packageId, DateTime shipDate, AccountDataModel account, AddressDataModel shippedFrom, AddressDataModel shippedTo)
-        {
-            this.OrderId = orderId;
-            this.PackageId = packageId;
-            this.ShipDate = shipDate;
-            this.Account = account;
-            this.ShippedFrom = shippedFrom;
-            this.ShippedTo = shippedTo;
-        }
-        
+    public OrderDataModel(int orderId, string packageId, DateTime shipDate, AccountDataModel account, AddressDataModel shippedFrom, AddressDataModel shippedTo)
+    {
+        this.OrderId = orderId;
+        this.PackageId = packageId;
+        this.ShipDate = shipDate;
+        this.Account = account;
+        this.ShippedFrom = shippedFrom;
+        this.ShippedTo = shippedTo;
     }
+        
+    public override string ToString()
+    {
+        return $"orderId: {OrderId}\nPackage Id (for tracking): {PackageId}\n{Account}Shipped from:\n{ShippedFrom}Shipped To:\n{ShippedTo}\n";
+    }
+}
