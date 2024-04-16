1e0c1008d630415e697b84b1975940da4f784ebe
Angie Zheng <111665983+angzheng22@users.noreply.github.com>
2024-04-15 19:23:14 -0500

    Updated DroneAccessor.cs
    Added GetDroneList()

--- backend/Accessors/Accessors/DroneAccessor.cs
+++ backend/Accessors/Accessors/DroneAccessor.cs
@@ -3,12 +3,15 @@ using System.Data.SqlTypes;
 using Managers.Models;
 using Microsoft.AspNetCore.Components.Web;
 
-namespace Accessors
+namespace Accessors.Accessors
+{
     public class DroneAccessor
     {
         public static Drone GetDrone(int droneId)
         {
             Drone drone = null;
+            Order droneOrder = null;
+            Depot currDepot = null;
 
             string query = "Select * from [Drone] where droneId = @DroneId";
 
@@ -19,13 +22,25 @@ namespace Accessors
                 connection.Open();
                 SqlCommand command = new SqlCommand(query, connection);
                 command.Parameters.AddWithValue("@DroneId", droneId);
-                SqlDataReader reader = commmand.ExecuteReader();
+                SqlDataReader reader = command.ExecuteReader();
                 if (reader.HasRows && reader.Read())
                 {
-                    string Status = reader.GetString(reader.GetOrdinal("Status"));
-                    string Destination = reader.GetString(reader.GetOrdinal("Destination"));
+                    string status = reader.GetString(reader.GetOrdinal("transit_status"));
+                    int orderId = reader.GetInt32(reader.GetOrdinal("orderId"));
+                    int? depotId = null; // Nullable depotId
+                    if (!reader.IsDBNull(reader.GetOrdinal("depotId")))
+                    {
+                        depotId = reader.GetInt32(reader.GetOrdinal("depotId"));
+                    }
 
-                    drone = new Drone(droneId, Status, Destination);
+                    if (depotId.HasValue)
+                    {
+                        currDepot = DepotAccessor.GetDepotWithDepotId(depotId.Value);
+                    }
+
+                    droneOrder = OrderAccessor.GetOrderWithOrderId(orderId);
+
+                    drone = new Drone(droneId, status, droneOrder, currDepot);
                 }
 
                 reader.Close();
@@ -41,5 +56,57 @@ namespace Accessors
 
             return drone;
         }
-    }
+        
+        public static List<Drone> GetDroneList()
+        {
+            List<Drone> droneList = new List<Drone>();
+            string query = "SELECT * FROM Drone";
 
+            SqlConnection connection = ConnectionAccessor.ConnectionAccessor.GetConnection();
+
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Depot currDepot = null;
+
+                        int droneId = reader.GetInt32(reader.GetOrdinal("droneId"));
+                        string transitStatus = reader.GetString(reader.GetOrdinal("transit_status"));
+                        int orderId = reader.GetInt32(reader.GetOrdinal("orderId"));
+                        int? depotId = null; // Nullable depotId
+                        if (!reader.IsDBNull(reader.GetOrdinal("depotId")))
+                        {
+                            depotId = reader.GetInt32(reader.GetOrdinal("depotId"));
+                        }
+
+                        if (depotId.HasValue)
+                        {
+                            currDepot = DepotAccessor.GetDepotWithDepotId(depotId.Value);
+                        }
+
+                        Order droneOrder = OrderAccessor.GetOrderWithOrderId(orderId);
+
+                        Drone d = new Drone(droneId, transitStatus, droneOrder, currDepot);
+                        droneList.Add(d);
+                    }
+                }
+
+                reader.Close();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"SQL Exception: {ex.Message}");
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return droneList;
+        }
+    }
+}
