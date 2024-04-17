b4cd22a82d9deff1e421a46c12d955541f2b007f
Angie Zheng <111665983+angzheng22@users.noreply.github.com>
2024-04-17 11:31:27 -0500

    Updated AddressAccessorTests.cs

--- backend/UnitTests/AddressAccessorTests.cs
+++ backend/UnitTests/AddressAccessorTests.cs
@@ -1,11 +1,24 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Accessors.DBModels;
+using Accessors.Accessors;
+
 namespace UnitTests
 {
     [TestClass]
     public class AddressAccessorTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Load_All_Addresses_From_Db()
         {
+            List<AddressDataModel> addressList = new List<AddressDataModel>();
+        }
+
+        [TestMethod]
+        public void Load_One_Address_From_Db_Using_Id()
+        {
+            AddressDataModel a = null;
         }
     }
 }
