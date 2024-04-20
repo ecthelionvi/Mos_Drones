4ce3123c15a950f1a3875550dd3a437e8b2a6940
Angie Zheng <111665983+angzheng22@users.noreply.github.com>
2024-04-20 14:40:22 -0500

    Update AccountAccessorUnitTests.cs

--- backend/UnitTests/AccessorTests/AccountAccessorUnitTests.cs
+++ backend/UnitTests/AccessorTests/AccountAccessorUnitTests.cs
@@ -1,22 +1,24 @@
 using System;
 using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Managers.Models;
 using Accessors.Accessors;
+using Accessors.DBModels;
 
-namespace backend{
-    [TestFixture]
+namespace UnitTests.AccessorTests
+{
+    [TestClass]
     public class GetAccountWithAccountIdAccessorTest
     {
-        [Test]
+        [TestMethod]
         public void ValidGetAccountWithAccountId()
         {
             throw new NotImplementedException();
             //Assert.AreEqual(true, GetAccountWithAccountId(1));
             //Assert.AreEqual(true, GetAccountWithAccountId(2));
             //Assert.AreEqual(true, GetAccountWithAccountId(3));
+        }
 
-        [Test]
+        [TestMethod]
         public void InvalidGetAccountWithAccountId()
         {
             throw new NotImplementedException();
@@ -24,6 +26,8 @@ namespace backend{
             //Assert.AreEqual(false, GetAccountWithAccountId(-2));
             //Assert.AreEqual(false, GetAccountWithAccountId(-3));
         }
+
+        [TestMethod]
         public void NullGetAccountWithAccountId()
         {
             throw new NotImplementedException();
@@ -32,18 +36,20 @@ namespace backend{
             //Assert.AreEqual(false, GetAccountWithAccountId());
         }
     }
-    [TestFixture]
+
+    [TestClass]
     public class GetAccountWithEmailAccessorTest
     {
-        [Test]
+        [TestMethod]
         public void ValidGetAccountWithEmail()
         {
             throw new NotImplementedException();
             //Assert.AreEqual(true, GetAccountWithEmail("123456@gmail.com"));
             //Assert.AreEqual(true, GetAccountWithEmail("ThisIsATest@gmail.com"));
             //Assert.AreEqual(true, GetAccountWithEmail("WhatAnEmail@gmail.com"));
+        }
 
-        [Test]
+        [TestMethod]
         public void InvalidGetAccountWithEmail()
         {
             throw new NotImplementedException();
@@ -51,12 +57,14 @@ namespace backend{
             //Assert.AreEqual(false, GetAccountWithEmail("IDkThisIsABadTest@gmail.com"));
             //Assert.AreEqual(false, GetAccountWithEmail("ThridTimesCharm@gmail.com"));
         }
+
+        [TestMethod]
         public void NullGetAccountWithEmail()
         {
             throw new NotImplementedException();
             //Assert.AreEqual(false, GetAccountWithEmail());
             //Assert.AreEqual(false, GetAccountWithEmail());
             //Assert.AreEqual(false, GetAccountWithEmail());
-        }
+        }     
     }
 }
