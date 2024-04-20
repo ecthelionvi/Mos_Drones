a81e1c449b14f477404f7d11e9c97f85ed3b1dcc
Angie Zheng <111665983+angzheng22@users.noreply.github.com>
2024-04-20 16:31:28 -0500

    Update AddressAccessorTests.cs

--- backend/UnitTests/AccessorTests/AddressAccessorTests.cs
+++ backend/UnitTests/AccessorTests/AddressAccessorTests.cs
@@ -1,24 +1,42 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Managers.Models;
+using Accessors.DBModels;
 using Accessors.Accessors;
 
-namespace UnitTests
+namespace UnitTests.AccessorTests
 {
     [TestClass]
     public class AddressAccessorTests
     {
         [TestMethod]
-        public void LoadAllAddressesFromDb()
+        public void LoadAllAddressesFromDb_ReturnsExpectedList()
         {
-            List<Address> addressList = new List<Address>();
+            List<AddressDataModel> expectedAddressList = new List<AddressDataModel>();
+            AddressDataModel a = new AddressDataModel(1, "Lincoln", "Nebraska", "68510", "4321 O Street");
+            AddressDataModel b = new AddressDataModel(2, "Lincoln", "Nebraska", "68521", "2468 North 27th Street");
+            AddressDataModel c = new AddressDataModel(3, "Lincoln", "Nebraska", "68516", "9876 Pine Lake Road");
+            AddressDataModel d = new AddressDataModel(4, "Lincoln", "Nebraska", "68505", "8020 Holdrege Street");
+            AddressDataModel e = new AddressDataModel(5, "Lincoln", "Nebraska", "68506", "1357 South 84th Street");
+
+            expectedAddressList.Add(a);
+            expectedAddressList.Add(b);
+            expectedAddressList.Add(c);
+            expectedAddressList.Add(d);
+            expectedAddressList.Add(e);
+
+            List<AddressDataModel> actualAddressList = AddressAccessor.GetAddressList();
+            CollectionAssert.AreEquivalent(expectedAddressList, actualAddressList);
         }
 
         [TestMethod]
-        public void LoadOneAddressFromDbUsingId()
+        public void LoadOneAddressFromDbUsingId_ReturnsCorrectAddress()
         {
-            Address a = null;
+            AddressDataModel expectedAddress = new AddressDataModel(1, "Lincoln", "Nebraska", "68510", "4321 O Street");
+
+            AddressDataModel actualAddress = AddressAccessor.GetAddress(1);
+
+            Assert.AreEqual(expectedAddress, actualAddress);
         }
     }
 }
