b33153ad2c5ce3a5b2e6847fb8374b1be4f29dd1
Angie Zheng <111665983+angzheng22@users.noreply.github.com>
2024-04-20 16:02:58 -0500

    Update OrderEngineUnitTests.cs

--- backend/UnitTests/EngineTests/OrderEngineUnitTests.cs
+++ backend/UnitTests/EngineTests/OrderEngineUnitTests.cs
@@ -1,27 +1,29 @@
 using System;
 using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Managers.Models;
-using Engines.Engines;
+using Engines.BizLogic;
 
-namespace Engines.BizLogic{
-    [TestFixture]
+namespace UnitTests.EngineTests
+{
+    [TestClass]
     public class OrderEngineTest
     {
-        [Test]
+        [TestMethod]
         public void ValidOrderEngine()
         {
             throw new NotImplementedException();
         }
 
-        [Test]
+        [TestMethod]
         public void InvalidOrderEngine()
         {
             throw new NotImplementedException();
         }
-        [Test]
+
+        [TestMethod]
         public void NullOrderEngine()
         {
             throw new NotImplementedException();
         }
     }
+}
