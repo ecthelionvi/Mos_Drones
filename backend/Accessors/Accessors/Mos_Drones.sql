29d01ec29cc19ea2ca3fcf4a72778015ac26a0a0
Angie Zheng <azheng2@huskers.unl.edu>
2024-04-29 13:06:04 -0500

    Update sql script

--- backend/Accessors/Accessors/Mos_Drones.sql
+++ backend/Accessors/Accessors/Mos_Drones.sql
@@ -9,7 +9,9 @@ CREATE TABLE [Address] (
   [city] nvarchar(255),
   [state] nvarchar(255),
   [zip] nvarchar(255),
-  [address_line] nvarchar(255)
+  [address_line] nvarchar(255),
+  [latitude] float,
+  [longitude] float
 )
 GO
 
@@ -69,3 +71,44 @@ GO
 
 ALTER TABLE [Drone] ADD CONSTRAINT drone_to_depot FOREIGN KEY ([depotId]) REFERENCES [Depot] ([depotId])
 GO
+
+--Creating dummy data
+insert into [Address] (city, state, zip, address_line, latitude, longitude) values ('Lincoln', 'Nebraska', '68510', '4321 O Street', 40.813380, -96.659490);
+insert into [Address] (city, state, zip, address_line, latitude, longitude) values ('Lincoln', 'Nebraska', '68521', '2468 North 27th Street', 40.837620, -96.682030);
+insert into [Address] (city, state, zip, address_line, latitude, longitude) values ('Lincoln', 'Nebraska', '68516', '9876 Pine Lake Road', 40.740760, -96.584900);
+insert into [Address] (city, state, zip, address_line, latitude, longitude) values ('Lincoln', 'Nebraska', '68505', '8020 Holdrege Street', 40.828020, -96.611440);
+insert into [Address] (city, state, zip, address_line, latitude, longitude) values ('Lincoln', 'Nebraska', '68506', '1357 South 84th Street', 40.799240, -96.606210);
+
+insert into [Account] (first_name, last_name, email, password, addressId, isAdmin) values ('Alfred', 'Van Arsdall', 'avanarsdall0@cocolog-nifty.com', 'uI7lq}{e0WU', 1, 1);
+insert into [Account] (first_name, last_name, email, password, addressId, isAdmin) values ('Beltran', 'Lillie', 'blillie1@imdb.com', 'hC7S>lx+N7a(?2>k', 2, 0);
+
+insert into [Order] (packageId, ship_date, deliveryDate, accountId, shipped_from, shipped_to) values ('4829170638572946', '2024-04-14 10:30:00', '2024-04-17 15:44:00', 1, 1, 2);
+insert into [Order] (packageId, ship_date, deliveryDate, accountId, shipped_from, shipped_to) values ('7294061538206194', '2024-04-01 17:12:00', '2024-04-05 08:15:00', 2, 2, 1);
+insert into [Order] (packageId, ship_date, deliveryDate, accountId, shipped_from, shipped_to) values ('7632987401568235', '2024-04-15 19:33:00', '2024-04-18 09:17:00', 1, 1, 2);
+
+insert into Depot (addressId) values (3);
+insert into Depot (addressId) values (4);
+
+insert into Drone (transit_status, orderId, depotId) values ('At Depot', 1, 1);
+insert into Drone (transit_status, orderId, depotId) values ('Delivered', 2, null);
+
+select * from Account;
+SELECT * FROM Account WHERE email = null;
+select * from Address;
+SELECT * FROM Address WHERE addressId = -1;
+SELECT Account.*, Address.city, Address.state, Address.zip, Address.address_line FROM Account JOIN Address ON Account.addressId = Address.addressId;
+select * from Address where Address.addressId = 1;
+
+SELECT * FROM Account WHERE email = 'avanarsdall0@cocolog-nifty.com';
+
+select * from [Order];
+SELECT * FROM [Order] WHERE orderId = 3;
+SELECT * FROM [Order] WHERE packageId = '4829170638572946';
+SELECT o.* FROM [Order] o JOIN [Account] a ON o.accountId = a.accountId WHERE o.accountId = 1;
+
+SELECT o.* FROM [Order] o JOIN [Account] a ON o.accountId = a.accountId WHERE a.email = 'avanarsdall0@cocolog-nifty.com';
+
+select * from Depot;
+SELECT * FROM Depot WHERE depotId = 1;
+select * from Drone;
+Select * from [Drone] where droneId = 2;
