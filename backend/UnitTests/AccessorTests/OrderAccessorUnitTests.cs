d41aaa25a09119e6b6571af2a406cc5a55214208
Angie Zheng <azheng2@huskers.unl.edu>
2024-05-06 17:50:47 -0500

    Updated OrderAccessor tests to include status

--- backend/UnitTests/AccessorTests/OrderAccessorUnitTests.cs
+++ backend/UnitTests/AccessorTests/OrderAccessorUnitTests.cs
@@ -15,7 +15,7 @@ namespace UnitTests.AccessorTests
             AddressDataModel shippedFrom = new AddressDataModel(1, "Lincoln", "Nebraska", "68510", "4321 O Street", coord);
             Coordinate coord2 = new Coordinate(40.83762, -96.68203);
             AddressDataModel shippedTo = new AddressDataModel(2, "Lincoln", "Nebraska", "68521", "2468 North 27th Street", coord2);
-            OrderDataModel expectedOrder = new OrderDataModel(1, "4829170638572946", shipDate, deliveryDate, 1, shippedFrom, shippedTo);
+            OrderDataModel expectedOrder = new OrderDataModel(1, "4829170638572946", shipDate, deliveryDate, 1, shippedFrom, shippedTo, "Delivered");
 
             OrderDataModel actualOrder = OrderAccessor.GetOrderWithOrderId(1);
             Assert.AreEqual(expectedOrder, actualOrder);
@@ -41,7 +41,7 @@ namespace UnitTests.AccessorTests
             AddressDataModel shippedFrom = new AddressDataModel(2, "Lincoln", "Nebraska", "68521", "2468 North 27th Street", coord);
             Coordinate coord2 = new Coordinate(40.81338, -96.65949);
             AddressDataModel shippedTo = new AddressDataModel(1, "Lincoln", "Nebraska", "68510", "4321 O Street", coord2);
-            OrderDataModel expectedOrder = new OrderDataModel(2, "7294061538206194", shipDate, deliveryDate, 2, shippedFrom, shippedTo);
+            OrderDataModel expectedOrder = new OrderDataModel(2, "7294061538206194", shipDate, deliveryDate, 2, shippedFrom, shippedTo, "Delivered");
 
             OrderDataModel actualOrder = OrderAccessor.GetOrderWithPackageId("7294061538206194");
             Assert.AreEqual(expectedOrder, actualOrder);
@@ -68,7 +68,7 @@ namespace UnitTests.AccessorTests
             AddressDataModel shippedFrom = new AddressDataModel(2, "Lincoln", "Nebraska", "68521", "2468 North 27th Street", coord);
             Coordinate coord2 = new Coordinate(40.74076, -96.5849);
             AddressDataModel shippedTo = new AddressDataModel(3, "Lincoln", "Nebraska", "68516", "9876 Pine Lake Road", coord2);
-            OrderDataModel order = new OrderDataModel(null, null, DateTime.Now, deliveryDate, 2, shippedFrom, shippedTo);
+            OrderDataModel order = new OrderDataModel(null, null, DateTime.Now, deliveryDate, 2, shippedFrom, shippedTo, "");
 
             int actualOrderId = OrderAccessor.InsertOrder(order);
 
