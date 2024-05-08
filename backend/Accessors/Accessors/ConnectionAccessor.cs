e27e6ffaab3829784b20359c2bb5f6a8a1e08a95
Jonny-Skelton <jskelton3@huskers.unl.edu>
2024-05-08 11:06:58 -0500

    conne

--- backend/Accessors/Accessors/ConnectionAccessor.cs
+++ backend/Accessors/Accessors/ConnectionAccessor.cs
@@ -8,98 +8,12 @@ namespace Accessors.ConnectionAccessor
     {
         public static SqlConnection GetConnection()
         {
-            //string connString = "Server=localhost,1433;Database=master;User Id=SA;Password=MyStrongPassword123;Integrated Security=False;;";
-            string connString = "Data Source=ANGIE-DELL-XPS\\SQLEXPRESS01; Initial Catalog=mos_drones; Integrated Security=True; MultipleActiveResultSets=True;";
+            string connString = "Server=localhost,1433;Database=master;User Id=SA;Password=MyStrongPassword123;Integrated Security=False;;";
+            //string connString = "Data Source=ANGIE-DELL-XPS\\SQLEXPRESS01; Initial Catalog=mos_drones; Integrated Security=True; MultipleActiveResultSets=True;";
             SqlConnection connection = new SqlConnection(connString);
             return connection;
         }
 
-        static void Main(string[] args)
-        {
-            // Testing AddressAccessor methods
-            List<AddressDataModel> addressList = AddressAccessor.GetAddressList();
-            Console.WriteLine("All Addresses: ");
-            foreach (AddressDataModel addr in addressList)
-            {
-                Console.WriteLine(addr);
-            }
-
-            Console.WriteLine("The address with addressId of 1: ");
-            AddressDataModel address1 = AddressAccessor.GetAddress(1);
-            Console.WriteLine(address1);
-
-            // Testing AccountAccessor methods
-            AccountDataModel account = AccountAccessor.GetAccountWithEmail("avanarsdall0@cocolog-nifty.com");
-            Console.WriteLine("The account with email avanarsdall0@cocolog-nifty.com:");
-            Console.WriteLine(account);
-
-            // Testing OrderAccessor methods
-            OrderDataModel order = OrderAccessor.GetOrderWithOrderId(3);
-            Console.WriteLine("The order with orderId of 3 is:");
-            Console.WriteLine(order);
-
-            OrderDataModel o = OrderAccessor.GetOrderWithPackageId("4829170638572946");
-            Console.WriteLine("The order with Package Id of 4829170638572946 is:");
-            Console.WriteLine(o);
-
-            //List<OrderDataModel> orderList = OrderAccessor.GetOrderListWithEmail("avanarsdall0@cocolog-nifty.com");
-            //Console.WriteLine("All Orders for email avanarsdall0@cocolog-nifty.com:");
-            //foreach (OrderDataModel or in orderList)
-            //{
-            //    Console.WriteLine(or);
-            //}
-
-            // Testing DepotAccessor methods
-            DepotDataModel depot = DepotAccessor.GetDepotWithDepotId(1);
-            Console.WriteLine("The depot with depotId of 1 is:");
-            Console.WriteLine(depot);
-
-            List<DepotDataModel> depotList = DepotAccessor.GetDepotList();
-            Console.WriteLine("All Depots:");
-            foreach (DepotDataModel d in depotList)
-            {
-                Console.WriteLine(d);
-            }
-
-            // Testing DroneAccessor methods
-            DroneDataModel drone = DroneAccessor.GetDrone(2);
-            Console.WriteLine("The drone with droneId of 2 is:");
-            Console.WriteLine(drone);
-
-            List<DroneDataModel> droneList = DroneAccessor.GetDroneList();
-            Console.WriteLine("All Drones: ");
-            foreach (DroneDataModel d in droneList)
-            {
-                Console.WriteLine(d);
-            }
-
-            // Inserting into the database
-            Coordinate c = new Coordinate(40.828410, -96.929760);
-            AddressDataModel address = new AddressDataModel(null, "Pleasant Dale", "Nebraska", "68423", "2468 North 10th Road", c);
-            int addressId = AddressAccessor.InsertAddress(address);
-            Console.WriteLine("The addressId of the selected/inserted address is " + addressId + "\n");
-
-            Coordinate coord = new Coordinate(40.817638, -96.699997);
-            AddressDataModel address2 = new AddressDataModel(null, "Lincoln", "Nebraska", "68588", "1400 R St", coord);
-            AccountDataModel acc = new AccountDataModel(null, "Angie", "Zheng", "azheng2@huskers.unl.edu", "password123", address2, false);
-            int accountId = AccountAccessor.InsertAccount(acc);
-            Console.WriteLine("The accountId of the selected/inserted account is " + accountId + "\n");
-
-            AccountDataModel acc2 = new AccountDataModel(null, "Angie", "Zheng", "angizheng4201@gmail.com", "password123", address2, false);
-            int accountId2 = AccountAccessor.InsertAccount(acc2);
-            Console.WriteLine("The accountId of the selected/inserted account is " + accountId2 + "\n");
-
-            DateTime deliveryDate = new DateTime(2024, 4, 23, 17, 02, 00);
-            DateTime shipDate = DateTime.Now;
-            Coordinate c1 = new Coordinate(40.817638, -96.699997);
-            AddressDataModel shippedFrom = new AddressDataModel(null, "Lincoln", "Nebraska", "68588", "1400 R St", c1);
-            Coordinate c2 = new Coordinate(40.828410, -96.929760);
-            AddressDataModel shippedTo = new AddressDataModel(null, "Pleasant Dale", "Nebraska", "68423", "2468 North 10th Road", c2);
-            OrderDataModel order2 = new OrderDataModel(null, null, shipDate, deliveryDate, 1, shippedFrom, shippedTo, "");
-            int orderId = OrderAccessor.InsertOrder(order2);
-            Console.WriteLine("The orderId of the inserted Order is " + orderId);
-
-            Console.ReadLine();
-        }
     }
 }
+
